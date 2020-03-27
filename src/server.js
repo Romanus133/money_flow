de6f768d617d6cdb132e6b1d76153444d9b5ce50
Romanus133 <iceman1305@gmail.com>
2020-03-27 20:34:39 +0300

    2_controllers

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 import process from 'process';
 import express from 'express';
+import user from './routers/UserRouter.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', async (request, response) => {
-    response.send('Root route');
-});
+app.use('/users', user);
 
 const server = app.listen(PORT, () => {
     console.log(`Server started on port ${server.address().port}`);
